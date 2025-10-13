f78055f84f8e482204ef629a6c7e395cef54cef8
Daniel Gabay <daniel221111@gmail.com>
2025-10-13 22:01:33 +0300

    Update calculator code

--- main.py
+++ main.py
@@ -25,33 +25,56 @@
 
 
 
-customer_name = "daniel gabay"
-item_name = "shirt"
-unit_price = 11.93
-quantity = 342
-is_first_time_customer = True 
-subtotal = unit_price * quantity 
-discount = subtotal * 0.10 if is_first_time_customer else 0 
+# customer_name = "daniel gabay"
+# item_name = "shirt"
+# unit_price = 11.93
+# quantity = 342
+# is_first_time_customer = True 
+# subtotal = unit_price * quantity 
+# discount = subtotal * 0.10 if is_first_time_customer else 0 
 
-is_express = True 
+# is_express = True 
 
-STANDARD_COST = 20 
-EXPRESS_COST = 40 
+# STANDARD_COST = 20 
+# EXPRESS_COST = 40 
 
-shipping_method = "express" if is_express else "standard"
-shipping_cost = EXPRESS_COST if is_express else STANDARD_COST
+# shipping_method = "express" if is_express else "standard"
+# shipping_cost = EXPRESS_COST if is_express else STANDARD_COST
 
-total = subtotal - discount + shipping_cost
-
-
-print(f"name: {customer_name}")
-print(f"item: {item_name}")
-print(f"unit price: {unit_price:.2f}")
-print(f"quantity: {quantity}")
-print(f"is first time customer? {'yes' if is_first_time_customer else 'no'}")
-print(f"discount: {discount:.2f}")
-print(f"shipping method: {shipping_method}")
-print(f"shipping cost: {shipping_cost:.2f}")
-print(f"total unit price: {total:.2f}")
+# total = subtotal - discount + shipping_cost
 
 
+# print(f"name: {customer_name}")
+# print(f"item: {item_name}")
+# print(f"unit price: {unit_price:.2f}")
+# print(f"quantity: {quantity}")
+# print(f"is first time customer? {'yes' if is_first_time_customer else 'no'}")
+# print(f"discount: {discount:.2f}")
+# print(f"shipping method: {shipping_method}")
+# print(f"shipping cost: {shipping_cost:.2f}")
+# print(f"total unit price: {total:.2f}")
+
+
+
+
+# birth_year = input('Enter your birth year: ')
+# age = 2025 - int(birth_year)
+# print(age)
+
+
+# num = float(input("enter a number "))
+# print (num)
+# a = float(input("first number "))
+# b = float(input(" second number "))
+# sum_result = a + b 
+# print (sum_result)
+
+# first = float(input("first number: "))
+# second = float(input("second number: "))
+# sum = float(first) + float(second)
+# print("sum: " + str(sum))
+
+first = float(input("first number: "))
+second = float(input("second number: "))
+sum = first + second 
+print (sum)
