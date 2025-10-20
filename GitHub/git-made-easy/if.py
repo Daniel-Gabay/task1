8402127e606670761bb66c3c4e503f803d911de6
Daniel <daniel221111@gmail.com>
2025-10-21 00:11:43 +0300

    your message

---
+++ GitHub/git-made-easy/if.py
@@ -0,0 +1,44 @@
+
+#שאלה 1
+#score = float(input("Enter your score: "))
+
+#if score < 1000:         # n > 0
+    #print("NOOB") # Positive
+#elif score <= 5000:        # n < 0
+    #print("pro") # Negative
+#else:
+    #print("legend") # Zero
+
+
+
+
+#שאלה ראשונה
+
+#number = float(input("Enter number: "))
+
+#if number > 0:
+    #print ("positive")
+#elif number < 0:
+    #print("negative")
+#else:
+    #print("zero")
+
+
+
+
+#שאלה 2
+#שאל אתה היוזר ֹבשביל ציון 
+
+user = float(input("enter grade 0-100 "))
+A = 90
+B = 85
+C = 70
+E = 55
+F = 40
+
+if (0 < user <= 100):
+    print("negative")
+if user >= A:
+    print("A")
+elif user <= B:
+    print("B")
--- daniel.py
+++ daniel.py
@@ -168,7 +168,6 @@
 
 
 
-
 # import math     
 
 # radius = float(input("Enter the radius of the circle: "))
